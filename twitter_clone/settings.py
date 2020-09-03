b85b4c088e3721005aba7a25777a0029f9588081
shuaib <wales149@gmail.com>
2020-09-03 16:59:54 -0700

    deploying on heroku

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -127,3 +128,5 @@ LOGIN_URL = 'login'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+
+django_heroku.settings(locals())
